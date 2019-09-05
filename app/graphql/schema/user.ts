ae8d334defbe755503c18eb5c8a9933e2e398a8f
Maxime JENNY <maxime.jenny@epitech.eu>
2019-09-05 23:23:40 +0200

    fix(docker-compose & type User): add type User in the Schema of GraphQL and little test on the docke

--- app/graphql/schema/user.ts
+++ app/graphql/schema/user.ts
@@ -4,4 +4,12 @@ export default gql`
   extend type Query {
     getUser: User!
   }
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    phone: String!
+  }
 `;
