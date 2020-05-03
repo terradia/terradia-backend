ae99b9e4815e7f7e595efebc875de3d37e75a5ee
Maxime JENNY <maxime.jenny@epitech.eu>
2020-05-03 03:35:42 +0200

    fix(Backend): Make that the product cannot have a unit.

--- app/graphql/schema/product.ts
+++ app/graphql/schema/product.ts
@@ -1,77 +1,77 @@
 import { gql } from "apollo-server";
 
 export default gql`
-    extend type Query {
-        getAllProducts: [Product]!
-        getProduct(id: ID!): Product!
-        getProductsByCompany(companyId: String): [Product]!
-        getProductsByCompanyByCategory(companyId: String): [Category]!
-        getAllUnits(referencesOnly: Boolean): [Unit]!
-        getUnit(id: ID, notation: String, name: String): Unit!
-    }
-    extend type Mutation {
-        createProduct(
-            name: String!
-            description: String!
-            companyId: String!
-            price: Float!
-            quantityForUnit: Int
-            unitId: String
-            companyProductsCategoryId: String
-        ): Product!
-        addCategoryToProduct(categoryName: String!, productId: String!): Product!
-        updateProduct(
-            productId: String
-            name: String
-            description: String
-            image: String
-            unitId: String
-            quantityForUnit: Float
-            price: Float
-        ): Product
-        deleteProduct(productId: String!): Int
-        updateProductsPosition(productsPositions: [ProductPosition!]): Boolean
-    }
-    #Type => ['addCategory', 'deleteCategory', 'moveCategory']
-    input ProductPosition {
-        productId: ID!
-        position: Int!
-        categoryId: ID
-        type: String
-    }
-    
-    type Product {
-        # Resouce related data
-        id: ID!
-        name: String!
-        description: String!
-        image: String
-        position: Int
+  extend type Query {
+    getAllProducts: [Product]!
+    getProduct(id: ID!): Product!
+    getProductsByCompany(companyId: String): [Product]!
+    getProductsByCompanyByCategory(companyId: String): [Category]!
+    getAllUnits(referencesOnly: Boolean): [Unit]!
+    getUnit(id: ID, notation: String, name: String): Unit!
+  }
+  extend type Mutation {
+    createProduct(
+      name: String!
+      description: String!
+      companyId: String!
+      price: Float!
+      quantityForUnit: Int
+      unitId: String
+      companyProductsCategoryId: String
+    ): Product!
+    addCategoryToProduct(categoryName: String!, productId: String!): Product!
+    updateProduct(
+      productId: String
+      name: String
+      description: String
+      image: String
+      unitId: String
+      quantityForUnit: Float
+      price: Float
+    ): Product
+    deleteProduct(productId: String!): Int
+    updateProductsPosition(productsPositions: [ProductPosition!]): Boolean
+  }
+  #Type => ['addCategory', 'deleteCategory', 'moveCategory']
+  input ProductPosition {
+    productId: ID!
+    position: Int!
+    categoryId: ID
+    type: String
+  }
 
-        createdAt: Date
-        updatedAt: Date
+  type Product {
+    # Resouce related data
+    id: ID!
+    name: String!
+    description: String!
+    image: String
+    position: Int
 
-        # Classification data
-        categories: [Category]
-        company: Company
-        companyProductsCategory: CompanyProductsCategory
+    createdAt: Date
+    updatedAt: Date
 
-        # Customers filled
-        reviews: [ProductReview]
-        averageMark: Float
-        numberOfMarks: Int
-        customerBasketProducts: [CartProduct]
+    # Classification data
+    categories: [Category]
+    company: Company
+    companyProductsCategory: CompanyProductsCategory
 
-        # Pricing
-        unit: Unit!
-        quantityForUnit: Float!
-        price: Float!
-    }
-    type Unit {
-        id: ID!
-        name: String!
-        notation: String!
-        referenceUnit: Unit
-        multiplicationFactor: Float
-    }
+    # Customers filled
+    reviews: [ProductReview]
+    averageMark: Float
+    numberOfMarks: Int
+    customerBasketProducts: [CartProduct]
+
+    # Pricing
+    unit: Unit
+    quantityForUnit: Float!
+    price: Float!
+  }
+  type Unit {
+    id: ID!
+    name: String!
+    notation: String!
+    referenceUnit: Unit
+    multiplicationFactor: Float
+  }
 `;
