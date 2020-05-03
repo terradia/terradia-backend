5632718fd3b153575e752eb4b1ef9623fc205f22
Damien <damsltc57@yahoo.fr>
2020-05-02 17:11:42 -0700

    fix(eslint): Remove useless jsx in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,19 +7,11 @@ module.exports =  {
     ],
     parserOptions:  {
         ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-        ecmaFeatures:  {
-            jsx:  true,  // Allows for the parsing of JSX
-        },
     },
     rules:  {
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unused-vars": "off",
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    },
-	settings:  {
-        node:  {
-            version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
-        },
-    },
+    }
 };
