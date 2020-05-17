bff91d3bcf38f02de3153ec831a71911afc41f2d
Damien <damsltc57@yahoo.fr>
2020-05-16 18:32:03 -0700

    refactor(models): remove unused imports

--- app/database/models/customer.model.ts
+++ app/database/models/customer.model.ts
@@ -17,11 +17,9 @@ import UserModel from "./user.model";
 import CompanyReviewModel from "./company-review.model";
 import CustomersFavoriteCompaniesModel from "./customers-favorite-companies.model";
 import CompanyModel from "./company.model";
-import CompanyAddressModel from "./customer-address.model";
 import ProductReviewModel from "./product-review.model";
 import CartModel from "./cart.model";
 import CustomerAddressModel from "./customer-address.model";
-import CompanyImagesModel from "./company-images.model";
 
 // Customer :
 // Contains the information of the customer, relating to his orders, payements, reviews etc...
