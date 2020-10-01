e69dbd5b6fd39b44f6cd5137333c729c08641efc
Damien <damsltc57@yahoo.fr>
2020-10-01 18:16:00 +0200

    fix(quotes): update quotes to doubles

--- app/graphql/schema/index.ts
+++ app/graphql/schema/index.ts
@@ -17,7 +17,7 @@ import tagCompanyCategorySchema from "./companyTag";
 import companyOpeningDaysSchema from "./companyOpeningDays";
 import companyDeliveryDaysSchema from "./companyDeliveryDays";
 import companyImagesSchema from "./companyImages";
-import paymentSchema from './payment';
+import paymentSchema from "./payment";
 
 const linkSchema = gql`
   scalar Date
