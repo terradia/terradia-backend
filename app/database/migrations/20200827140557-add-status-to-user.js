016fb6eed8c3169468e9cb712c8f5bd68b7dc2be
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2020-10-30 15:57:18 +0100

    style(cleaning): delete comments

--- app/database/migrations/20200827140557-add-status-to-user.js
+++ app/database/migrations/20200827140557-add-status-to-user.js
@@ -25,55 +25,6 @@ exports.down = function(db) {
   return db.removeColumn("Users", "archivedAt");
 };
 
-/*exports.up = function(db) {
-  return db
-    .runSql(
-      `CREATE TYPE enum_users_status AS ENUM ('VALID', 'UNVALID', 'ARCHIVED');`
-    )
-    .then(() => {
-      return db.addColumn("Users", "status", {
-        type: "enum_users_status",
-        allowNull: false
-      });
-    })
-    .then(() => {
-      db.runSql(
-        `UPDATE "public"."Users" SET status = 'VALID' WHERE "public"."Users"."validated" = '1';`
-      );
-    })
-    .then(() =>
-      db.runSql(
-        `UPDATE "public"."Users" SET status = 'UNVALID' WHERE "public"."Users"."validated" = '0';`
-      )
-    )
-    .then(() => db.removeColumn("Users", "validated"));
-};
-
-exports.down = function(db) {
-  return db
-    .addColumn("Users", "validated", {
-      type: "boolean",
-      allowNull: true
-    })
-    .then(() =>
-      db.runSql(
-        `UPDATE "public"."Users" SET validated = '1' WHERE "public"."Users"."status" = 'VALID';`
-      )
-    )
-    .then(
-      () =>
-        `UPDATE "public"."Users" SET validated = '0' WHERE "public"."Users"."status" = 'UNVALID';`
-    )
-    .then(
-      () =>
-        `UPDATE "public"."Users" SET validated = '0' WHERE "public"."Users"."status" = 'ARCHIVED';`
-    )
-    .then(() => db.removeColumn("Users", "status"))
-    .then(() => {
-      db.runSql("DROP TYPE IF EXISTS enum_users_status");
-    });
-};*/
-
 exports._meta = {
   version: 1
 };
