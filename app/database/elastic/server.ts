7a37936cabb1bb6bba0a3cbdffcaf65700085050
maximejenny <maxime.jenny@epitech.eu>
2020-11-13 13:40:23 +0100

    fix(Elastic Config): fixed config of elastic

--- app/database/elastic/server.ts
+++ app/database/elastic/server.ts
@@ -5,7 +5,7 @@ import ElasticSearch from "@elastic/elasticsearch";
  * @type {Client}
  */
 const client = new ElasticSearch.Client({
-  node: process.env.ELASTIC_HOST
+  node: process.env.ELASTIC_HOST || "http://localhost:9200"
 });
 
 export default client;
